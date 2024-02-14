d902bcce1673ab407c13ddf219ab557ab59119bf
Aditya-1208 <adityaagr.201ec103@nitk.edu.in>
2024-02-14 09:41:21 +0530

    initialize SQLAlchemy db object and import in app factory function

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from dotenv import load_dotenv
 load_dotenv('.env')
 from config import Config
+from app.extensions import db
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    db.init_app(app)
 
     @app.route('/')
     def home():
