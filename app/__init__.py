8da88d615dc1dbd0ad8d005b05633d7750cedd88
Aditya-1208 <adityaagr.201ec103@nitk.edu.in>
2024-02-14 09:33:26 +0530

    create config and apply to flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from dotenv import load_dotenv
+load_dotenv('.env')
+from config import Config
 
-def create_app():
+
+def create_app(config_class=Config):
     app = Flask(__name__)
+    app.config.from_object(config_class)
 
     @app.route('/')
     def home():
         return 'Hello World!</h1>'
-
     return app
---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASSWORD')
+db_host = os.environ.get('DB_HOST')
+db_port = os.environ.get('DB_PORT')
+db_name = os.environ.get('DB_NAME')
+
+DATABASE_URI = f"mysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = DATABASE_URI or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
