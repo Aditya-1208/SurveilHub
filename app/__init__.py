02dac215b142a10c02dbdcd3070be7e1fbc5c917
Aditya-1208 <adityaagr.201ec103@nitk.edu.in>
2024-02-17 13:08:18 +0530

    import flask-migrate and models into main app

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,10 @@ from flask import Flask
 from dotenv import load_dotenv
 load_dotenv('.env')
 from config import Config
+from flask_migrate import Migrate
 from app.extensions import db
+from app.models.camera import Camera
+from app.models.object_detection import ObjectDetection
 
 
 def create_app(config_class=Config):
@@ -10,6 +13,7 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     @app.route('/')
     def home():
