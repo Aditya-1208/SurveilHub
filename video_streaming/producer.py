93f533c8868c3dd204b4ba17ce8c56509aa932ed
Prasanna <prasannapm416@gmail.com>
2024-03-13 18:20:11 +0530

    working subprocess and publishing

--- video_streaming/producer.py
+++ video_streaming/producer.py
@@ -8,50 +8,45 @@ load_dotenv('.env')
 
 topic = "videostreaming"
 
-def publish_video(video_file):
+def publish_video(url):
     """
-    Publish given video file to a specified Kafka topic. 
+    Publish video file specified in environment variable VIDEO_PATH to a specified Kafka topic.
     Kafka Server is expected to be running on the localhost. Not partitioned.
-    
-    :param video_file: path to video file <string>
     """
     # Start up producer
     producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
     # Open file
-    video = cv2.VideoCapture(video_file)
+    video = cv2.VideoCapture(url)
     success, image = video.read()
     print('publishing video...')
     count = 0
 
-    while(video.isOpened()):
+    while video.isOpened():
+        success, image = video.read()
+        if not success:
+            break
+
         frame = cv2.imencode('.jpg', image)
         if len(frame) <= 1:
             print("no data")
             continue
 
         frame_bytes = frame[1]
-        success, image = video.read()
         count += 1
         if (count - 1) % int(os.getenv('FRAME_RATE')) == 0:
             producer.send(topic, bytearray(frame_bytes))
-            print("Frame published to Kafka topic:", topic)  # Print statement to indicate frame publication
+            print("Frame published to Kafka topic:", topic)
+
 
     video.release()
     print('publish complete')
 
-
 if __name__ == '__main__':
-    """
-    Producer will publish to Kafka Server a video file given as a system arg. 
-    Otherwise it will default by streaming webcam feed.
-    """
-
-    parser = argparse.ArgumentParser(description='Publish video to Kafka topic')
-    parser.add_argument('--video_path', type=str, help='Path to video file')
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url', help='URL of the video file')
     args = parser.parse_args()
 
-    video_path = args.video_path 
-    publish_video(video_path)
+    url = args.url
+    publish_video(url)
 
